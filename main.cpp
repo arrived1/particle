30c56fd1da149bb3f004f27f796e88469367bfd4
arrived1 <arrived1@gmail.com>
2014-03-13 00:23:51 +0100

    basic movement rdy

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@
 
 int main()
 {
-
+	ParticleSystem<float2> system;
 }
--- particleSystem.h
+++ particleSystem.h
@@ -3,6 +3,16 @@
 #include <vector>
 #include <algorithm>
 
+struct float2
+{
+	float2(float x_ = 0, float y_ = 0)
+		: x(x_), y(y_)
+	{}
+	
+	float x;
+	float y;
+};
+
 struct float3
 {
 	float3(float x_ = 0, float y_ = 0, float z_ = 0)
@@ -39,7 +49,74 @@ std::ostream & operator<<(std::ostream &out, const float3 &f)
 typedef std::vector<float3> vector3;
 typedef std::vector<float4> vector4;
 
+#define SQR(x) ((x)*(x))
+
+template<typename T>
 class ParticleSystem
 {
+	typedef std::vector<T> vecType;
+	vecType vel;
+	vecType nextVel;
+	vecType pos;
+	vecType prevPos;
+	vecType nextPos;
+	float mass;
+	float radius;
+	unsigned particles;
+	unsigned step;
+
 public:
+	ParticleSystem(unsigned particlesAmount = 2) 
+		: mass(1.f),
+		radius(0.03f),
+		particles(particlesAmount),
+		step(0u)
+	{
+		initlize();
+	}
+
+	void initlize() {
+
+		vel.push_back(T(4.f, 0.f));
+		pos.push_back(T(-1.f, 0.f));
+		prevPos.push_back(pos[0]);
+
+		vel.push_back(T(-4.f, 0.f));
+		pos.push_back(T(1.f, 0.f));
+		prevPos.push_back(pos[1]);
+	}
+
+
+
+
+	void prepareMoveEuler(unsigned i, T acceleration, float dt) {
+		nextVel[i] = vel[i] + acceleration * dt;
+		nextPos[i] = pos[i] + nextVel[i] * dt;
+	}
+
+
+	void prepareMoveVerlet(unsigned i, T acceleration, float dt) {
+		if(step == 0)
+			prepareMoveEuler(i, acceleration, dt);
+
+		nextPos[i] = 2.f * pos[i] - prevPos[i] + acceleration * SQR(dt);
+		nextVel[i] = (nextPos[i] - prevPos[i]) / (2.f * dt);
+	}
+
+
+	void prepareMove(unsigned i, T force, float dt) {
+		T acceleration = force / mass;
+
+		prepareMoveVerlet(i, acceleration, dt);
+	}
+
+
+	void move(unsigned i) {
+		prevPos[i] = pos[i];
+		pos[i] = nextPos[i];
+		vel[i] = nextVel[i];
+		step++;
+	}
+
+
 };
